a8d6bbe2a7086ad789edeecd6ac1390578061de4
Praneeth Ravuri <prav2500@gmail.com>
2024-03-26 09:31:56 -0400

    Database connection and user schema

---
+++ models/userModel.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+}
+
+export interface IUserDocument extends IUser, Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userSchema = new mongoose.Schema<IUserDocument>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    }
+},
+    {
+        timestamps: true
+    })
+
+
+const User: Model<IUserDocument> = mongoose.models?.User || mongoose.model("User", userSchema)
+
+export default User
