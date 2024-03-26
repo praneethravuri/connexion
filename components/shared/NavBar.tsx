b67d5e93578202113b1b1dee3d0992457bca57c2
Praneeth Ravuri <prav2500@gmail.com>
2024-03-26 16:02:03 -0400

    navbar logo names

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -19,26 +19,27 @@ const NavBar = () => {
 
     return (
         <>
-        <div className='top-0 z-30 flex w-full items-center justify-between bg-black px-6 py-6'>
-            <Logo />
-            <div className='flex space-x-4'>
-                {navItems.map(({ href, icon: Icon, label }) => {
-                    const isActive = pathname === href;
-                    const isNotLogoutIcon = Icon !== LogOut;
-                    return (
-                        
-                        <div key={label} className={`p-2 rounded ${isActive ? 'bg-zinc-800' : ''} ${isNotLogoutIcon ? 'max-sm:hidden' : ''}`} >
-                            <Link href={href} passHref>
-                                <div className="cursor-pointer">
-                                    <Icon color="#fff"/>
-                                </div>
-                            </Link>
-                        </div>
-                    );
-                })}
+            <div className='top-0 z-30 flex w-full items-center justify-between bg-black px-6 py-6'>
+                <Logo />
+                <div className='flex space-x-4'>
+                    {navItems.map(({ href, icon: Icon, label }) => {
+                        const isActive = pathname === href;
+                        const isNotLogoutIcon = Icon !== LogOut;
+                        return (
+
+                            <div key={label} className={`p-2 rounded ${isActive ? 'bg-zinc-800' : ''} ${isNotLogoutIcon ? 'max-sm:hidden' : ''}`} >
+                                <Link href={href} passHref>
+                                    <div className="flex items-center cursor-pointer space-x-1">
+                                        <Icon color="#fff" />
+                                        <p className='text-sm max-md:hidden'>{label}</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </div>
-        <hr className="border-t border-zinc-800 mx-auto w-11/12" />
+            <hr className="border-t border-zinc-800 mx-auto w-11/12" />
         </>
     );
 }
