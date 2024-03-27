5fe68506d40c4b246b7def456a020df38fd0a244
Praneeth Ravuri <prav2500@gmail.com>
2024-03-27 13:23:15 -0400

    Left side bar

--- app/(root)/homepage/page.tsx
+++ app/(root)/homepage/page.tsx
@@ -1,12 +1,14 @@
 
 import Bottombar from '@/components/shared/Bottombar'
+import LeftSideBar from '@/components/shared/LeftSideBar'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='bg-black h-screen w-full'>
+    <section className='bg-black h-screen w-full'>
+      <LeftSideBar />
       <Bottombar />
-    </div>
+    </section>
     
   )
 }
---
+++ components/shared/LeftSideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftSideBar = () => {
+    return (
+        <section className='hidden xl:block h-screen w-1/6 border-r-2 border-zinc-900'>LeftSideBar</section>
+    )
+}
+
+export default LeftSideBar
