d038a48888d9e00b215aba417fea4bf6411999ae
Praneeth Ravuri <prav2500@gmail.com>
2024-03-27 15:03:00 -0400

    Update LeftSideBar.tsx

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -27,8 +27,8 @@ const LeftSideBar = () => {
                 <div className='space-y-2 mt-3'>
                     {resourceItems.map(({ icon: Icon, label }) => (
                         <div key={label} className="rounded-lg p-3 flex items-center gap-xs min-w-0 shrink space-x-2 text-white hover:bg-zinc-800 cursor-pointer">
-                            <Icon className='h-5 w-5 leading-7 mb-1' />
-                            <span className='text-lg mb-1'>{label}</span>
+                            <Icon className='h-5 w-5 leading-7 ' />
+                            <span className='text-lg'>{label}</span>
                         </div>
                     ))}
                 </div>
@@ -38,11 +38,15 @@ const LeftSideBar = () => {
                 <div className='space-y-2 mt-3'>
                     {legalItems.map(({ icon: Icon, label }) => (
                         <div key={label} className="rounded-lg p-3 flex items-center gap-xs min-w-0 shrink space-x-2 text-white hover:bg-zinc-800 cursor-pointer">
-                        <Icon className='h-5 w-5 leading-7 mb-1' />
-                        <span className='text-lg mb-1'>{label}</span>
-                    </div>
+                            <Icon className='h-5 w-5 leading-7 ' />
+                            <span className='text-lg'>{label}</span>
+                        </div>
                     ))}
                 </div>
+            </div>
+
+            <div className="copyright text-center">
+                <p className='text-xs text-zinc-800 px-md'>Connexion, Inc. &copy; 2024. All rights reserved.</p>
             </div>
         </section>
     );
