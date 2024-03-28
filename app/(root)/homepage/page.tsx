563dce7a80d36094d5d217b10c0d119eb87a077d
Praneeth Ravuri <prav2500@gmail.com>
2024-03-28 11:17:39 -0400

    Update page.tsx
    Homepage now shows the mixed content

--- app/(root)/homepage/page.tsx
+++ app/(root)/homepage/page.tsx
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import LeftSideBar from '@/components/shared/LeftSideBar';
 import RightSideBar from '@/components/shared/RightSideBar';
-import { ThumbsDown, ThumbsUp, MessageSquare, Share } from 'lucide-react';
+import { ThumbsDown, ThumbsUp, MessageSquare, Share, Dot } from 'lucide-react';
 import Post, { IPostDocument } from '@/app/(models)/postModel';
+import Link from 'next/link';
 
 const HomePage = () => {
   const [posts, setPosts] = useState<IPostDocument[]>([]);
@@ -34,10 +35,6 @@ const HomePage = () => {
     return date.toLocaleDateString('en-US', options);
   };
 
-
-
-
-
   return (
     <section className='bg-black h-screen w-full flex'>
       <LeftSideBar currentPage="Homepage" />
@@ -47,30 +44,27 @@ const HomePage = () => {
             <hr className="border-t border-zinc-800 mx-auto my-4" />
             <article className="bg-black text-white p-4 rounded-lg hover:bg-gray-800">
               <div className="mb-2 flex justify-between items-center">
-                <span className="text-base text-zinc-500 hover:text-zinc-200 cursor-pointer">{post.userName}</span>
+                <div className='flex'>
+                  <Link href='/' className="text-base text-zinc-500 hover:text-zinc-200 cursor-pointer">{post.userName}</Link>
+                  <Dot />
+                  <Link href='/' className="text-base text-zinc-500 hover:text-zinc-200 cursor-pointer">{post.community}</Link>
+                </div>
+                {/* <span className="text-base text-zinc-500 hover:text-zinc-200 cursor-pointer">{post.userName}</span> */}
                 <span className="text-base text-zinc-500">{formatDate(post.createdAt)}</span>
               </div>
               <h3 className="text-2xl font-bold mb-2">{post.title}</h3>
               {post.contentType === "text" ? (
-                <p>{post.content}</p>
-              ) : (
-                <img src={`${post.content}?random=${post.id}`} alt={post.title} width={1000} height={400} className="max-w-full h-auto rounded-lg" />
-              )}
+                <p>{post.contentText}</p>
+              ) : post.contentType === "image" ? (
+                <img src={post.contentImageURL} alt={post.title} width={1000} height={400} className="max-w-full h-auto rounded-lg" />
+              ) : post.contentType === "mix" ? (
+                <>
+                  
+                  <img src={post.contentImageURL} alt={post.title} width={1000} height={400} className="max-w-full h-auto rounded-lg" />
+                  <p className='mt-3'>{post.contentText}</p>
+                </>
+              ) : null}
 
-              {/* <div className="buttons flex space-x-4 mt-3">
-                <div className='rounded-lg flex items-center space-x-2'>
-                  <ThumbsUp className='h-5 w-5 cursor-pointer' color="#fff" /><span>{post.likeCount}</span>
-                </div>
-                <div className='rounded-lg flex items-center space-x-2'>
-                  <ThumbsDown className='h-5 w-5 cursor-pointer' color="#fff" /><span>{post.dislikeCount}</span>
-                </div>
-                <div className='rounded-lg flex items-center space-x-2'>
-                  <MessageSquare className='h-5 w-5 cursor-pointer' color="#fff" /><span>{post.commentCount}</span>
-                </div>
-                <div className='rounded-lg flex items-center space-x-2'>
-                  <Share className='h-5 w-5 cursor-pointer' color="#fff" />
-                </div>
-              </div> */}
               <div className="buttons flex space-x-4 mt-3">
                 <div className='rounded-lg flex items-center space-x-2'>
                   <ThumbsUp className='h-5 w-5 cursor-pointer' color="#fff" /><span>12</span>
