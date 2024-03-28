8b7c8eaf1f1f06221dc364f4b451d18e0592c92a
Praneeth Ravuri <prav2500@gmail.com>
2024-03-28 14:51:37 -0400

    Community pages setup

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import LeftSideBar from '@/components/shared/LeftSideBar'
+import RightSideBar from '@/components/shared/RightSideBar'
 
 const Community = () => {
   return (
-    <section>
-        <h1>Community List</h1>
-        <p>C1</p>
-        <p>C2</p>
-        <p>C3</p>
+    <section className='bg-black h-screen w-full flex'>
+      <LeftSideBar currentPage='community-main' />
+      <main className="main-content flex-1 overflow-y-auto px-20 pt-6">You are at main com</main>
+      <RightSideBar />
     </section>
   )
 }
