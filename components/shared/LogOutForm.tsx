12682034fcb33922854b37d43164b01d5931b543
Praneeth Ravuri <prav2500@gmail.com>
2024-03-31 18:17:58 -0400

    Updated navbar

--- components/shared/LogOutForm.tsx
+++ components/shared/LogOutForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { logout } from '@/lib/actions'
+import { Button } from '@/components/ui/button'
 
 const LogOutForm = () => {
   return (
     <form action={logout}>
-        <button>Logout</button>
+        <Button variant="ghost">Logout</Button>
     </form>
   )
 }
--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import { Home, Send, Handshake, User, BadgePlus, LogOut } from 'lucide-react';
+import { Home, Handshake, User, BadgePlus, LogOut } from 'lucide-react';
 import Logo from './Logo';
-// import { usePathname } from "next/navigation";
 import LogOutForm from './LogOutForm';
 import { getSession } from '@/lib/actions';
 
@@ -11,27 +10,24 @@ const navItems = [
     { href: '/community', icon: Handshake, label: 'Community' },
     { href: '/create', icon: BadgePlus, label: 'Create' },
     { href: '/profile', icon: User, label: 'Profile' },
-    { href: '/login', icon: LogOut, label: 'Log Out' },
 ];
 
 const NavBar = async () => {
-    // const pathname = usePathname();
     const session = await getSession();
     console.log(session);
 
     return (
         <>
-            <div className='top-0 z-30 flex w-full items-center justify-between bg-black px-6 py-6'>
+            <div className='top-0 z-30 flex w-full items-center justify-between bg-black px-4 py-3'>
                 <Logo />
                 <div className='flex space-x-4'>
                     {navItems.map(({ href, icon: Icon, label }) => {
-                        // const isActive = pathname === href;
                         const isNotLogoutIcon = Icon !== LogOut;
                         return (
-                            // className={`p-2 rounded ${isActive ? 'bg-gray-800' : ''} ${isNotLogoutIcon ? 'max-sm:hidden' : ''}`
                             <div key={label} className='p-2 rounded-lg max-sm:hidden' >
-                                <Link href={href} passHref>
+                                <Link href={href} passHref className='flex items-center space-x-2 hover:bg-zinc-800 p-3 rounded-lg'>
                                     <Icon color="#fff" />
+                                    <p>{label}</p>
                                 </Link>
                             </div>
                         );
