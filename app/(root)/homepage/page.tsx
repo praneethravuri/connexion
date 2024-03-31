42f9071146766790d32a92ebe140c780ea1b616c
Praneeth Ravuri <prav2500@gmail.com>
2024-03-31 18:01:47 -0400

    Protected homepage route

--- app/(root)/homepage/page.tsx
+++ app/(root)/homepage/page.tsx
@@ -3,15 +3,23 @@ import LeftSideBar from '@/components/shared/LeftSideBar';
 import RightSideBar from '@/components/shared/RightSideBar';
 import Bottombar from '@/components/shared/Bottombar';
 import UserPosts from '@/components/shared/UserPosts';
+import { getSession } from '@/lib/actions';
+import { redirect } from 'next/navigation';
 
-const HomePage = () => {
-  const user = "Anitha";
+const HomePage = async () => {
+
+  const session = await getSession();
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
+  const user = session.fullName;
 
   return (
     <section className='bg-black h-screen w-full flex'>
       <LeftSideBar />
       <main className="main-content flex-1 overflow-y-auto px-20 pt-6">
-        <p className='text-3xl'>Welcome 👋, &nbsp;{user}</p>
+        <p className='text-3xl'>Welcome, &nbsp;{user}👋</p>
         <UserPosts />
       </main>
       <RightSideBar />
